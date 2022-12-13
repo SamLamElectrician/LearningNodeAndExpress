d1af391f0e90ec132c3437635ef26e435da38739
Sammy <sammylam_1@hotmail.com>
2022-12-13 16:20:23 -0500

    packages

---
+++ 4-lodash.js
@@ -0,0 +1,8 @@
+//importing external modules
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]];
+//lodash method to flatten array
+const newItems = _.flattenDeep(items);
+
+console.log(newItems);
