d76d0bb27c0c3551e624c76f827c2c27c01a4a82
Sammy <sammylam_1@hotmail.com>
2022-12-20 20:16:13 -0500

    notes

--- 1-Node-tutorial/10-http-stream.js
+++ 1-Node-tutorial/10-http-stream.js
@@ -1,3 +1,4 @@
+//import modules
 var http = require('http');
 var fs = require('fs');
 
@@ -5,10 +6,14 @@ http
 	.createServer(function (req, res) {
 		// const text = fs.readFileSync('./content/big.txt', 'utf8')
 		// res.end(text)
+		//creates the stream in utf8 encoding
 		const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+		//listening for open
 		fileStream.on('open', () => {
+			//pipe method --> push from read stream to write stream
 			fileStream.pipe(res);
 		});
+		//error handling
 		fileStream.on('error', (err) => {
 			res.end(err);
 		});
--- 1-Node-tutorial/9-streams.js
+++ 1-Node-tutorial/9-streams.js
@@ -1,13 +1,20 @@
+//takes from the fs module
 const { createReadStream } = require('fs');
 
-// default 64kb
+// default size of the buffer 64kb
 // last buffer - remainder
-// highWaterMark - control size
+// highWaterMark --> control size of buffer
 // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
 // const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+
+//invocs createReadStraeam
 const stream = createReadStream('./content/big.txt');
 
+//events is in this package
+//listens for data, console logs results in callback function
 stream.on('data', (result) => {
+	//what do do with the data
 	console.log(result);
 });
+//error handling
 stream.on('error', (err) => console.log(err));
