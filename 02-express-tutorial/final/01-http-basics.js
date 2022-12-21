0e2b859f5aff210b2ba9e89a9f398e818d4ed54b
Sammy <sammylam_1@hotmail.com>
2022-12-21 12:48:28 -0500

    note

--- 02-express-tutorial/final/01-http-basics.js
+++ 02-express-tutorial/final/01-http-basics.js
@@ -1,26 +1,28 @@
-const http = require('http')
-
+//calls http for importing http module, built in
+const http = require('http');
+//setting up server
+//http built in method
 const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>home page</h1>')
-    res.end()
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
-})
-
-server.listen(5000)
+	// console.log(req.method)
+	const url = req.url;
+	// home page
+	if (url === '/') {
+		res.writeHead(200, { 'content-type': 'text/html' });
+		res.write('<h1>home page</h1>');
+		res.end();
+	}
+	// about page
+	else if (url === '/about') {
+		res.writeHead(200, { 'content-type': 'text/html' });
+		res.write('<h1>about page</h1>');
+		res.end();
+	}
+	// 404
+	else {
+		res.writeHead(404, { 'content-type': 'text/html' });
+		res.write('<h1>page not found</h1>');
+		res.end();
+	}
+});
+//listening on a port:5000
+server.listen(5000);
