ab084a9bf1f1fdfaa4fa8bcb890eeba38426f9bf
Sammy <sammylam_1@hotmail.com>
2022-12-22 11:45:41 -0500

    express

--- app.js
+++ app.js
@@ -4,26 +4,25 @@ const http = require('http');
 //http built in method
 const server = http.createServer((req, res) => {
 	// console.log(req.method)
-	console.log('User hit server');
-	// const url = req.url;
-	// // home page
-	// if (url === '/') {
-	// 	res.writeHead(200, { 'content-type': 'text/html' });
-	// 	res.write('<h1>home page</h1>');
-	// 	res.end();
-	// }
-	// // about page
-	// else if (url === '/about') {
-	// 	res.writeHead(200, { 'content-type': 'text/html' });
-	// 	res.write('<h1>about page</h1>');
-	// 	res.end();
-	// }
-	// // 404
-	// else {
-	// 	res.writeHead(404, { 'content-type': 'text/html' });
-	// 	res.write('<h1>page not found</h1>');
-	// 	res.end();
-	// }
+	const url = req.url;
+	// home page
+	if (url === '/') {
+		//provides header info, metadata about response
+		//res.writeHead(statusCode{KV pair})
+		res.writeHead(200, { 'content-type': 'text/html' });
+		//content being passed
+		res.write('<h1>home page</h1>');
+		//always call res.end to end response
+		res.end();
+	} else if (url === '/about') {
+		res.writeHead(200, { 'content-type': 'text/html' });
+		res.write('<h1>about page</h1>');
+		res.end();
+	} else {
+		res.writeHead(404, { 'content-type': 'text/html' });
+		res.write('<h1>page not found</h1>');
+		res.end();
+	}
 });
 //listening on a port:5000 for request
 server.listen(5000);
