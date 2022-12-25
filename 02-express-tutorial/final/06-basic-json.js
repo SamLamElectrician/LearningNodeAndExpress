2a22968925b08f32c1e15a6a23415de38937455a
Sammy <sammylam_1@hotmail.com>
2022-12-24 21:45:05 -0500

    notes

--- 02-express-tutorial/final/06-basic-json.js
+++ 02-express-tutorial/final/06-basic-json.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
-app.get('/', (req, res) => {
-  res.json(products)
-})
+//import express
+const express = require('express');
+// uses express method
+const app = express();
 
+const { products } = require('./data');
+//http method for default
+app.get('/', (req, res) => {
+	//method name is json
+	//sends back a JSON response and converted to json string using json.stringify()
+	res.json(products);
+});
+
+// app.get('/', (req, res) => {
+// 	res.json([{name: 'john'}, {name:'susan'}]);
+// });
+
+//listen for server
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+	console.log('Server is listening on port 5000....');
+});
